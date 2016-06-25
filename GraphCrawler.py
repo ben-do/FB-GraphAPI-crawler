a32feed2823c5420b954a1e3184fd5ce4dd30bfe
ben <simulatedmsn@gmail.com>
2016-06-25 23:11:46 +0800

    get next paging data

--- GraphCrawler.py
+++ GraphCrawler.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import json
+import os
 import requests
 import time
 import datetime
@@ -12,11 +13,13 @@ class GraphCrawler:
         self.access_token = setting['access_token']
         self.page_name = page_name
         self.base_url = "https://graph.facebook.com/v2.6/%s/" % page_name
+        self.post_data = []
 
     def get_access_token(self):
         return self.access_token
 
     def get_posts(self, since=None, until=None):
+        # set time range
         since_unix = None
         until_unix = None
         if since is not None:
@@ -24,19 +27,53 @@ class GraphCrawler:
         if until is not None:
             until_unix = time.mktime(datetime.datetime.strptime(until, "%Y-%m-%d").timetuple())
 
+        # first request
         payload = {'access_token': self.access_token,
                    'since': since_unix,
                    'until': until_unix
                    }
         r = requests.get(self.base_url + 'posts', params=payload)
+        posts = json.loads(r.text)
+
+        for d in posts["data"]:
+            self.post_data.append(d)
+
+        # get next page data
+        while self.have_paging(posts) is True:
+            print "have next page, try to get new post"
+            posts = self.load_next(posts)
+            for d in posts["data"]:
+                self.post_data.append(d)
+
         return json.loads(r.text)
 
-    def save_posts(self, since=None, until=None):
-        posts = self.get_posts(since, until)
-        for post in posts["data"]:
+    def save_posts(self):
+        for post in self.post_data:
             file_name = post['id']
-            my_file = open("data/" + file_name, 'w')
-            content = post['id'].encode('utf-8') + "\n" + post['created_time'].encode('utf-8') + "\n" + post['message'].encode('utf-8')
-            my_file.write(content)
-            my_file.close()
+            if not os.path.isdir("data/"):
+                os.mkdir("data/")
 
+            if "message" in post:
+                my_file = open("data/" + file_name, 'w')
+                content = post['id'].encode('utf-8') + "\n" + post['created_time'].encode('utf-8') + "\n" + post['message'].encode('utf-8')
+                my_file.write(content)
+                my_file.close()
+
+    def have_paging(self, posts):
+        if "paging" in posts:
+            return True
+        else:
+            return False
+
+    def load_next(self, posts):
+        next_url = posts["paging"]["next"]
+        next_r = requests.get(next_url)
+        # print next_r.text
+        next_posts = json.loads(next_r.text)
+        return next_posts
+
+    def write_post_into_file(self, file_name="post_data.txt"):
+        with open(file_name, 'w') as outfile:
+            json.dump(self.post_data, outfile)
+
+    
--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 # coding=utf-8
 
 import GraphCrawler as GC
-
+import json
 
 if __name__ == '__main__':
     crawler = GC.GraphCrawler(page_name='littlelifer')
     print crawler.get_access_token()
-    # posts = crawler.get_posts(since="2016-06-20", until="2016-06-25")
-
-    # print posts.text
+    posts = crawler.get_posts(since="2016-04-07", until="2016-04-08")
+    print crawler.post_data
+    # crawler.write_post_into_file()
+    # my_file = open("post_data", 'w')
+    # my_file.write(str(crawler.post_data))
+    # my_file.close()
+    # with open('post_data.txt', 'w') as outfile:
+    #     json.dump(crawler.post_data, outfile)
+    # print posts
     # print posts["data"]
     # for post in posts["data"]:
     #     print post['id']
@@ -16,5 +22,5 @@ if __name__ == '__main__':
     # for post in posts.text['data']:
     #     print post.data.id
 
-    posts = crawler.save_posts(since="2016-06-20", until="2016-06-25")
+    posts = crawler.save_posts()
 
